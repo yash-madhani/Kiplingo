06feeace949782b54dfd05a6a665a6a395622680
Yash <yashmadhani2810@gmail.com>
2024-06-29 16:22:05 +0530

    Addedd herosection and worked on influencer page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Navbar from "@/components/Navbar";
-
+import Herosection from "@/components/HeroSection";
 
 export default function Home() {
+  
+
   return (
     <div className="">
-      Test
+      <Herosection/>
     </div>
   );
 }
