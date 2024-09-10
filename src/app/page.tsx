6c08f35beb80993ee1fed0a3c4f1ce73c5dd2d31
Yash <yashmadhani2810@gmail.com>
2024-09-10 21:26:20 +0530

    RE First commit, restart of the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import Herosection from "@/components/HeroSection";
-import { Testimonials } from "@/components/Testimonials";
-import { ServicesSection } from "@/components/ServicesSection";
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="h-full w-full dark:bg-slate-950 dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-        {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-slate-950 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-          <Herosection />
-          <ServicesSection />
-          <Testimonials />
-        </div>
-      </div>
-    </div>
+    <>
+    Under development
+    </>
   );
 }
